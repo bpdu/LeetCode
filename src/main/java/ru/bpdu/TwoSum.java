e1689808432c958855b3fc7da83f827c10db7969
Alexey Rumyantsev <me@bpdu.ru>
2023-01-26 22:07:13 +0300

    Use two cycles for finding the numbers

--- src/main/java/ru/bpdu/TwoSum.java
+++ src/main/java/ru/bpdu/TwoSum.java
@@ -2,6 +2,17 @@ package ru.bpdu;
 
 public class TwoSum {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                if (nums[j] == nums[i] + target) {
+                    return new int[] {i, j};
+                }
+            }
+
+        }
+        return new int[] {Integer.MIN_VALUE, Integer.MIN_VALUE};
     }
 }
