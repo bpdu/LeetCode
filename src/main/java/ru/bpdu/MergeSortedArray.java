2ef277abd247a49e098883eb747df91c23dc53d5
Alexey Rumyantsev <me@bpdu.ru>
2023-02-13 14:31:41 +0300

    Add MergeSortedArray

--- src/main/java/ru/bpdu/MergeSortedArray.java
+++ src/main/java/ru/bpdu/MergeSortedArray.java
@@ -1,5 +1,9 @@
 package ru.bpdu;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * You are given two integer arrays nums1 and nums2, sorted in non-decreasing order,
  * and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
@@ -13,7 +17,9 @@ public class MergeSortedArray {
     public static void main(String[] args) {
     }
 
-    public static int[] merge(int[] nums1, int m, int[] nums2, int n) {
-
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        List<Integer> list = new ArrayList<>();
+        list.addAll(Arrays.copyOf(nums1, m, int[]));
+        list.addAll(nums2);
     }
 }
