87b7f04c6ff831a45ac97e1f9f83c6bdbcb44245
alexr <me@bpdu.ru>
2023-02-24 13:45:52 +0300

    Initial commit

--- src/main/java/ru/bpdu/BinaryTreeInorderTraversal.java
+++ src/main/java/ru/bpdu/BinaryTreeInorderTraversal.java
@@ -7,6 +7,27 @@ public class BinaryTreeInorderTraversal {
     public static void main(String[] args) {
     }
 
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
     public List<Integer> inorderTraversal(TreeNode root) {
 
     }
+}
